0b7ce254a88b563dd7d89e5ab6b3d84ad6fc3aec
sash1401 <tharan_88@yahoo.com>
2019-08-11 11:28:32 +0530

    Fianl code for demo

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 let request = require('request');
-// define routes here..
-
 var bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
@@ -12,28 +11,19 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-/*app.post('/submit-student-data', function (req, res) {
-    var name = req.body.firstName + ' ' + req.body.lastName;
-    console.log(req);
-    request({
-        method: 'GET',
-        url: '',
-        headers: {}
-    },function(error,response,body){
-        console.log("Response body ",body)
-        res.status(200).send(body);
-        res.send(body);
-    })
-    
-    
-});*/
 
 app.post('/submit-student-data', function (req, res) {
     var name = req.body.firstName + ' ' + req.body.lastName;
+    var submitAction ='';
+    if( req.body.submitAction =="Register")
+        submitAction="Registered"
+    else
+        submitAction="Updated"
     console.log("url is : ",process.env.URL);
     console.log("name"+name);
     console.log("dept:"+req.body.department)
     console.log(JSON.stringify(req.body))
+  
     request(
         {
         method: 'POST',
@@ -46,7 +36,7 @@ app.post('/submit-student-data', function (req, res) {
         console.log("Response.body",response.body);
         console.log("error",error)
         if(body.errorMessage==null|| body.errorMessage=='' ){
-            res.send("The Student "+name+ " is registered successfully");
+            res.send("<h2>The Student "+name+ " is "+submitAction+" successfully</h2>");
             
         }else{
             res.send("Error Occured.Please try again ")
